2fee93039dc99adf5433dd207c61479bf9048916
Neveous <badyrova.kristin@mail.ru>
2023-03-21 20:01:08 +0800

    JaCoCo added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxIfMaxFirst() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -16,4 +16,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 55, 3, 8, 6, 11, 11, 12};
+        long expected = 55;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
